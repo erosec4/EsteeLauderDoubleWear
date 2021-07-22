0f34954d2266b7f699d3409bb4d39d31b4779f70
Emma Carpenetti <emmac4164@gmail.com>
2021-07-22 00:46:49 -0400

    LearnView slideshow attempt (failed)

---
+++ EsteeLauderDoubleWear/DataCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  DataCollectionViewCell.swift
+//  EsteeLauderDoubleWear
+//
+//  Created by Emma Carpenetti on 7/22/21.
+//
+
+import UIKit
+
+class DataCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var img: UIImageView!
+    
+}
--- EsteeLauderDoubleWear/LearnViewController.swift
+++ EsteeLauderDoubleWear/LearnViewController.swift
@@ -9,21 +9,32 @@ import UIKit
 
 class LearnViewController: UIViewController {
 
+    @IBOutlet weak var sliderCollectionView: UICollectionView!
+    @IBOutlet weak var pageView: UIPageControl!
+    
+    var imageNames = [ "EL5",
+                       "EL8.2",
+                       "EL9"
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
+}
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+extension LearnViewController: UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return imageNames.count
     }
-    */
-
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as? DataCollectionViewCell
+        cell?.img.image = UIImage(named: imageNames[indexPath.row])
+        
+        return cell!
+    }
 }
