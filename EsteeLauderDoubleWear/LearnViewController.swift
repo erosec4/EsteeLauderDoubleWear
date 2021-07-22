d5baab853c08a2f7d2ed9bd16fc36a15db899298
Emma Carpenetti <emmac4164@gmail.com>
2021-07-22 16:28:20 -0400

    LearnView link button works, static image gallery added

--- EsteeLauderDoubleWear/LearnViewController.swift
+++ EsteeLauderDoubleWear/LearnViewController.swift
@@ -7,28 +7,17 @@
 
 import UIKit
 
-class LearnViewController: UIViewController { //, UICollectionViewDelegate, UICollectionViewDataSource {
-    
-    
-    @IBOutlet weak var collectionView: UICollectionView!
-    @IBOutlet weak var imageInCell: UIImageView!
-    var imageNames = ["EL5", "EL9", "EL8.2", "EL2", "EL12", "EL16"]
+class LearnViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        /* collectionView.delegate = self
-        collectionView.dataSource = self
     }
     
-    func numberOfSections(in collectionView: UICollectionView) -> Int {
-        return 6
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
-        
-        //cell.imageInCell = imageNames[indexPath.row] */
+    @IBAction func linkClicked(_ sender: Any) {
+        if let url = URL(string: "https://www.esteelauder.com/product/643/22830/product-catalog/makeup/face/foundation/double-wear/stay-in-place-foundation#/shade/0N1-Alabaster") {
+                    UIApplication.shared.open(url, options: [:], completionHandler: nil)
+                 }
     }
     
 }
